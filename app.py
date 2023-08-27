cf939496a9696f1d04ae05017a51a73db7b9eb10
Lucas Bubner <lkbubner@proton.me>
2023-08-27 22:56:35 +0930

    Experimental image filtering

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ ProfanityAPI WSGI server in Flask.
 
 from flask import Flask, request, abort, make_response
 from profanity_check import predict, predict_prob
+import opennsfw2 as n2
+import uuid
 
 app = Flask(__name__)
 
@@ -21,3 +23,22 @@ def f():
     })
     res.headers["Access-Control-Allow-Origin"] = "*"
     return res
+
+@app.route("/img", methods=["POST"])
+def g():
+    img = request.data
+    # Write into a file path
+    rand_path = str(uuid.uuid4())
+    with open(f"/tmp/{rand_path}.png", "wb") as f:
+        f.write(img)
+    prediction = n2.predict_image(f"/tmp/{rand_path}.png")
+    res = make_response({
+        "nsfw": prediction
+    })
+    res.headers["Access-Control-Allow-Origin"] = "*"
+    return res
+
+
+@app.route("/video", methods=["POST", "OPTIONS"])
+def h():
+    raise NotImplementedError("Video processing is not implemented yet.")
