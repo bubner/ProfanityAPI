a3357ec50a02edcae91f3bf19b8c51b7760cd267
Lucas Bubner <lkbubner@proton.me>
2023-08-28 10:01:11 +0930

    Revert to commit f29d01b9c13cf335cc13ca9df80dc9712121a326

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ ProfanityAPI WSGI server in Flask.
 
 from flask import Flask, request, abort, make_response
 from profanity_check import predict, predict_prob
-import opennsfw2 as n2
-import uuid
 
 app = Flask(__name__)
 
@@ -23,22 +21,3 @@ def f():
     })
     res.headers["Access-Control-Allow-Origin"] = "*"
     return res
-
-@app.route("/img", methods=["POST"])
-def g():
-    img = request.data
-    # Write into a file path
-    rand_path = str(uuid.uuid4())
-    with open(f"/tmp/{rand_path}.png", "wb") as f:
-        f.write(img)
-    prediction = n2.predict_image(f"/tmp/{rand_path}.png")
-    res = make_response({
-        "nsfw": prediction
-    })
-    res.headers["Access-Control-Allow-Origin"] = "*"
-    return res
-
-
-@app.route("/video", methods=["POST"])
-def h():
-    raise NotImplementedError("Video processing is not implemented yet.")
